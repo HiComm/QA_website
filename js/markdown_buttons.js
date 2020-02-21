a3d264f0325d894c5fa7ad5b76ae818280869a33
安間愛莉 <ayasuma@hioki.co.jp>
2020-02-21 15:04:46 +0900

    イタリックボタン実装完了！

--- js/markdown_buttons.js
+++ js/markdown_buttons.js
@@ -17,3 +17,18 @@ function bold_button(){
     editor.focus();
     editor.setSelectionRange(pos+2, pos+2);
 }
+function italic_button(){
+    var editor = document.getElementById("input");
+    var pos = editor.selectionEnd;
+    var sentence = editor.value;
+    var len      = sentence.length;
+    var before   = sentence.substr(0, pos);
+    var word     = '**';
+    var after    = sentence.substr(pos, len);
+
+    sentence = before + word + after;
+    editor.value = sentence;
+
+    editor.focus();
+    editor.setSelectionRange(pos+1, pos+1);
+}
