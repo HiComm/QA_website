d0de282b9f248aafbbb84428d2e2aff5fd0a3f96
矢澤 幸也 <y.yazawa@hioki.co.jp>
2020-02-21 16:19:38 +0900

    fix: コードをちょっと修正．

--- js/markdown.js
+++ js/markdown.js
@@ -1,18 +1,18 @@
 // textareaでのTABキー入力
-document.getElementById('input').onkeydown = function(e) {
-    if (e.keyCode === 9) {
-        e.preventDefault();
-        var elem = e.target;
-        var val = elem.value;
-        var pos = elem.selectionStart;
-        elem.value = val.substr(0, pos) + '\t' + val.substr(pos, val.length);
-        elem.setSelectionRange(pos + 1, pos + 1);
-    };
-};
+window.onload = function() {
+    document.getElementById('input').onkeydown = function(e) {
+        if (e.keyCode === 9) {
+            e.preventDefault();
+            var elem = e.target;
+            var val = elem.value;
+            var pos = elem.selectionStart;
+            elem.value = val.substr(0, pos) + '\t' + val.substr(pos, val.length);
+            elem.setSelectionRange(pos + 1, pos + 1);
+        }
+    }
+}
 
-
-document.getElementById('input').onkeyup = function(e) {
+function markdown2html() {
     document.getElementById('result').innerHTML =
     marked(document.getElementById('input').value);
-};
-
+}
