9bb6b151e5634cf8da091b9244cb55b98c35c2a2
T Matsutake <t.matsutake@hioki.co.jp>
2020-03-05 10:38:04 +0900

    mod: bold/italic ボタンを範囲選択時も機能するように変更

--- js/markdown_buttons.js
+++ js/markdown_buttons.js
@@ -4,31 +4,47 @@ function test(){
 
 function bold_button(){
     var editor = document.getElementById("input");
-    var pos = editor.selectionEnd;
+    var pos_start = editor.selectionStart;
+    var pos_end = editor.selectionEnd;
     var sentence = editor.value;
     var len      = sentence.length;
-    var before   = sentence.substr(0, pos);
-    var word     = '****';
-    var after    = sentence.substr(pos, len);
+
+    var before   = sentence.substr(0, pos_start);
+    var word_pre = '**';
+    var content  = sentence.substr(pos_start, pos_end-pos_start);
+    var word_post = '**';
+    var after    = sentence.substr(pos_end, len);
     
-    sentence = before + word + after;
+    sentence = before + word_pre + content + word_post + after;
     editor.value = sentence;
     
     editor.focus();
-    editor.setSelectionRange(pos+2, pos+2);
+    if(pos_start == pos_end){
+        editor.setSelectionRange(pos_end+2, pos_end+2);
+    }else{
+        editor.setSelectionRange(pos_end+content.length+2, pos_end+content.length+2);
+    }
 }
 function italic_button(){
     var editor = document.getElementById("input");
-    var pos = editor.selectionEnd;
+    var pos_start = editor.selectionStart;
+    var pos_end = editor.selectionEnd;
     var sentence = editor.value;
     var len      = sentence.length;
-    var before   = sentence.substr(0, pos);
-    var word     = '**';
-    var after    = sentence.substr(pos, len);
 
-    sentence = before + word + after;
+    var before   = sentence.substr(0, pos_start);
+    var word_pre = '*';
+    var content  = sentence.substr(pos_start, pos_end-pos_start);
+    var word_post = '*';
+    var after    = sentence.substr(pos_end, len);
+    
+    sentence = before + word_pre + content + word_post + after;
     editor.value = sentence;
-
+    
     editor.focus();
-    editor.setSelectionRange(pos+1, pos+1);
+    if(pos_start == pos_end){
+        editor.setSelectionRange(pos_end+1, pos_end+1);
+    }else{
+        editor.setSelectionRange(pos_end+content.length+1, pos_end+content.length+1);
+    }
 }
