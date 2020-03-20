0a023878f876c04bc01bbf0f867af0fe6bb7e06f
yuckey <snowshop@yazawayukiya-no-MacBook-Pro.local>
2020-03-20 21:45:01 +0900

    add: bold(Bボタン)のショートカット（Shift＋a）をお試しで追加．markdownチートシートの修正.

--- js/markdown.js
+++ js/markdown.js
@@ -1,5 +1,11 @@
-// textareaでのTABキー入力
 window.onload = function() {
+    inputTabInTextarea();
+    addShortcutKey();
+    removeShortcutKey();
+}
+
+// textareaでのTABキー入力
+function inputTabInTextarea() {
     document.getElementById('input').onkeydown = function(e) {
         if (e.keyCode === 9) {
             e.preventDefault();
@@ -9,6 +15,50 @@ window.onload = function() {
             elem.value = val.substr(0, pos) + '    ' + val.substr(pos, val.length);
             elem.setSelectionRange(pos + 4, pos + 4);
         }
+    }
+}
+
+/*
+// focusの変更を監視
+function addFocusListener() {
+    addEventListener("focus", validFocusOnTextarea, true);
+}
+
+// focusがmarkdownのテキストエリアにあるかをチェック
+function validFocusOnTextarea() {
+    var focused = document.activeElement;
+    if (focused === document.getElementById('input')) addShortcutKey();
+    else removeShortcutKey();
+}
+*/
+
+// textareaのfocusになったらmarkdownのショートカットキーの追加
+function addShortcutKey() {
+    document.getElementById('input').onfocus = function() {
+        // "B"ボタン
+        shortcut.add("Shift+a", function() {
+            $('#mde-buttons div p:nth-child(1) button').click();
+        });
+        // I ボタン(** **)
+        // H ボタン(#)
+        // list-ul ボタン(*)
+        // list-ol ボタン(1.)
+        // linkボタン([]http://)
+        // imageボタン(![](hhtp://)
+    }
+}
+
+// textareaのfocusが外れたらmarkdownのショートカットを削除　
+function removeShortcutKey() {
+    document.getElementById('input').onblur = function() {
+        // "B"ボタン
+        shortcut.remove("Shift+a");
+        // I ボタン(** **)
+        // H ボタン(#)
+        // list-ul ボタン(*)
+        // list-ol ボタン(1.)
+        // linkボタン([]http://)
+        // imageボタン(![](hhtp://)
     }
 }
 
