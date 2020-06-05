75f8dab6d2ff89a1194227dd707f5ea1f780e4e4
t.matsutake <t.matsutake@hioki.co.jp>
2020-06-05 16:43:04 +0900

    add hash function

--- php/post_question.php
+++ php/post_question.php
@@ -1,6 +1,13 @@
 <?php
     #include("./accessDB.php");
+    $ctx = hash_init('fnv1a64');
+    hash_update($ctx, $_POST["userid"]);
+
     print_r($_POST["userid"]);
     print_r($_POST["title"]);
     print_r($_POST["body"]);
+    print_r(hash_final($ctx));
+
+
+
 ?>
